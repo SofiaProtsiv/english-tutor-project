61a79b43ecce4ce0dadc786a37a8124261bb3569
Sofia Protsiv <prochivsofia@gmail.com>
2024-03-20 22:12:30 +0200

    feat: initial commit, add base structure

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/form';
+import "./js/header";
+import "./js/localization";
+import "./js/modal";
+import "./js/opportunities-slider";
+import "./js/reviews-slider";
+import "./js/pop-up";
