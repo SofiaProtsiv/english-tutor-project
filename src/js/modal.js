76c077561c4ffd0824a95e7919d6d566fd82eb2e
totsamii-alex <144161343+TasamayaAnastasiia@users.noreply.github.com>
2024-03-24 15:05:23 +0100

    feat(modal):ET-113 improve js for modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,102 +3,12 @@ const buttonClick = document.querySelector(".btn-connect");
 const modalWindow = document.querySelector(".mdl-box");
 const contentForm = document.querySelector(".mdl-content")
 const butClose = document.querySelector(".close");
-const inputUserName = document.querySelector(".username");
-const butSubmit = document.querySelector(".but-submit");
-const inputUserPhone = document.querySelector(".phone");
 const formUser = document.querySelector(".forma-for-connect");
-const labelText = document.querySelector("label-inpt");
+const formElements = Array.from(formUser.elements);
 const token = "6077606622:AAG6g12itzLvnsQfazmk9-oBfkHb1kflQYk";
 let idCzat = -1002080915692;
 
-
-formUser.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    checkName();
-    checkPhone();
-
-    async function sendMessage(message) {
-        await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-                chat_id: idCzat,
-                text: message
-            })
-        .then(response => { 
-            succes();
-            response.status;
-        })
-        .catch(error => {
-            errorr();
-            console.error("Error in sending sms:", error)
-        });
-    }
-        
-        const formData = new FormData(document.querySelector('.forma-for-connect'));
-        const userName = formData.get("username");
-        const userPhone = formData.get("phone");
-        const education = formData.get("education");
-        const comment = formData.get("comment");
-        
-        const message = `
-            Нова заявка:
-    1) Им'я: ${userName};
-    2) Телефон: ${userPhone};
-    3) Формат навчання: ${education};
-    4) Коментар: ${comment.split("").length > 0 ? comment : "Without comments"};
-        `;
-        sendMessage(message);
-});
-
-
-//check input name//
-
-async function checkName() {
-
-    if(inputUserName.value.trim() === "") {
-        inputUserName.classList.add("red");
-    }
-    else {
-        inputUserName.classList.remove("red");
-    }
-}
-
-// //check input phone//
-
-async function checkPhone() {
-
-    if(inputUserPhone.value.trim() === "" || !(inputUserPhone.value.trim().includes("+")) || inputUserPhone.value.trim().split('').length < 12) {
-        inputUserPhone.classList.add("red");
-    }
-    else {
-        inputUserPhone.classList.remove("red");
-    }
-}
-
-//succes feedback//
-
-async function succes() {
-        let str = `
-                <h2 class="title-feedback">See you soon!</h2>
-                <p class="text-feedback">Ваші дані були успішно відправлені. Будь ласка, очікуйте: я зв'яжуся з Вами якнайшвидше для обговорення деталей.</p>`;
-
-        contentForm.innerHTML = str;
-        contentForm.style.flexDirection = "column";
-        contentForm.style.gap = "24px";
-}
-
-//succes error//
-
-async function errorr() {
-        let str = `
-                <h2 class="title-feedback">Error</h2>
-                <p class="text-feedback">На жаль, на сайті сталася помилка і Ваші дані не<br>були відправлені. Спробуйте, будь ласка, пізніше.</p>`;
-
-        contentForm.innerHTML = str;
-        contentForm.style.flexDirection = "column";
-        contentForm.style.gap = "24px";
-}
-
-buttonClick.addEventListener("click", function () {
+buttonClick.addEventListener("click", function () { 
     modalWindow.style.display = "block";
 });
 
@@ -106,15 +16,66 @@ butClose.addEventListener("click", function () {
     modalWindow.style.display = "none";
 });
 
-inputUserName.addEventListener("input", (e) => {
-    checkName();
+formUser.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    checkInput();
+
+    const formData = new FormData(e.target);
+    const userName = formData.get("username");
+    const userPhone = formData.get("phone");
+    const education = formData.get("education");
+    const comment = formData.get("comment");
+        
+    const message = `
+        Нова заявка:
+        1) Ім'я: ${userName};
+        2) Телефон: ${userPhone};
+        3) Формат навчання: ${education};
+        4) Коментар: ${comment ? comment : "Без коментарів"};
+    `;
+
+    sendMessage(message);
 });
 
-inputUserPhone.addEventListener("input", (e) => {
-    checkPhone();
-});
+async function sendMessage(message) {
+    try {
+        await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+            chat_id: idCzat,
+            text: message
+        });
+        feedbackMessage(true);
+    } 
+    catch (error) {
+        feedbackMessage(false);
+        console.error("Error in sending sms:", error);
+    }
+}
 
-butSubmit.addEventListener("click", (e) => {
-    checkName();
-    checkPhone();
-})
+//check input//
+
+async function checkInput() {
+    for (let i = 0; i < formElements.length; i++) {
+        const isPhoneField = formElements[i].classList.contains("phone");
+        const isTextComField = formElements[i].classList.contains("text-com");
+
+        if (!isTextComField) {
+        formElements[i].classList.toggle("red", formElements[i].value.trim() === "" || (isPhoneField && (!formElements[i].value.includes("+") || formElements[i].value.length < 12)));
+        }
+    }
+}
+
+//succes feedback//
+
+async function feedbackMessage(success) {
+        let str = `
+                <h2 class="title-feedback">${success ? `See you soon!` : `Error`}</h2>
+                <p class="text-feedback">${success ? `Ваші дані були успішно відправлені. Будь ласка, очікуйте: я зв'яжуся з Вами якнайшвидше для обговорення деталей.` : `На жаль, на сайті сталася помилка і Ваші дані не були відправлені. Спробуйте, будь ласка, пізніше.`}</p>`;
+
+        contentForm.innerHTML = str;
+        contentForm.style.flexDirection = "column";
+        contentForm.style.gap = "24px";
+}
+
+formElements.forEach(field => {
+    field.addEventListener("input", checkInput);
+});
