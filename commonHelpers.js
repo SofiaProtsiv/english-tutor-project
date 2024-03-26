0ae82500e94dc2741c95991eb20889ea6a4a82b1
SofiaProtsiv <71260387+SofiaProtsiv@users.noreply.github.com>
2024-03-26 11:36:45 +0000

    Deploying to gh-pages from @ SofiaProtsiv/english-tutor-project@734c6e230a51cee9ab50d3f2fb9c564431345540 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,69 +1,30 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))o(n);new MutationObserver(n=>{for(const s of n)if(s.type==="childList")for(const m of s.addedNodes)m.tagName==="LINK"&&m.rel==="modulepreload"&&o(m)}).observe(document,{childList:!0,subtree:!0});function r(n){const s={};return n.integrity&&(s.integrity=n.integrity),n.referrerPolicy&&(s.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?s.credentials="include":n.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(n){if(n.ep)return;n.ep=!0;const s=r(n);fetch(n.href,s)}})();const u=document.querySelector("#HEADER_JS"),h=()=>{u.innerHTML=`
- <div class="header-container container">
-   <a
-     href="./index.html"
-     target="_self"
-     class="logo"
-     aria-label="link on main page"
-   >
-     <svg class="icon-logo">
-       <use href="./images/icons-sprite.svg#icon-logo"></use>
-     </svg>
-   </a>
-   <button
-     id="HEADER_MENU_JS"
-     class="menu-button"
-     title="open menu"
-     type="button"
-   >
-     <svg class="icon-menu">
-       <use href="./images/icons-sprite.svg#icon-menu"></use>
-     </svg>
-     <svg class="icon-menu-close">
-       <use href="./images/icons-sprite.svg#icon-menu-close"></use>
-     </svg>
-   </button>
-   <section class="menu-container">
-     <div class="menu-content">
-       <ul id="MENU_NAV_JS" class="menu-nav">
-       </ul>
-        <div class="custom-select-small" id="CUSTOM_SELECT_SMALL_JS"></div>
-       <div class="custom-select" id="CUSTOM_SELECT_JS">
-       </div>
-     </div>
-     <nav class="menu-social" id="MENU_SOCIAL_JS">
-       <svg class="icon-horisontal-line">
-         <use href="./images/icons-sprite.svg#icon-horisontal-line"></use>
-       </svg>
-     </nav>
-   </section>
- </div>
-  `};u&&h();const g=document.querySelector("#HEADER_MENU_JS");g&&(g.onclick=()=>{u.classList.toggle("header-menu-open")});const f=document.querySelector("#MENU_NAV_JS");f&&(f.onclick=e=>{e.target.tagName==="A"&&u.classList.remove("header-menu-open")});const l=document.querySelector("#MENU_NAV_JS"),y=()=>` <li class="menu-item" data-section="about-me"  type="button">
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))s(n);new MutationObserver(n=>{for(const o of n)if(o.type==="childList")for(const u of o.addedNodes)u.tagName==="LINK"&&u.rel==="modulepreload"&&s(u)}).observe(document,{childList:!0,subtree:!0});function r(n){const o={};return n.integrity&&(o.integrity=n.integrity),n.referrerPolicy&&(o.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?o.credentials="include":n.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(n){if(n.ep)return;n.ep=!0;const o=r(n);fetch(n.href,o)}})();const S={ua:{header:{menu:{about_me:"ПРО МЕНЕ",quote:"ВАРТІСТЬ",reviews:"ВІДГУКИ",contacts:"КОНТАКТИ"}},hero:{},"about-me":{},advice:{},quote:{},opportunities:{},requirements:{},workflow:{},assessment:{},reviews:{},footer:{},modal:{},"pop-up":{},formats:{title:"German title",button:{test:"Зписатися"}}},en:{header:{menu:{about_me:"ABOUT ME",quote:"QUOTE",reviews:"REVIEWS",contacts:"CONTACTS"}},hero:{},"about-me":{},advice:{},quote:{},opportunities:{},requirements:{},workflow:{},assessment:{},reviews:{},footer:{},modal:{},"pop-up":{},formats:{title:"Formats studing",button:{test:"Lessons!!!"}}}};function b(e,t){return t.split(".").reduce((s,n)=>s?s[n]:void 0,e)}function y(e){document.querySelectorAll("[data-lang]").forEach(r=>{const s=r.getAttribute("data-lang"),n=b(S[e],s);n&&(r.textContent=n)})}const v=document.querySelector("#HEADER_JS"),d=document.querySelector("#HEADER_MENU_JS");d&&(d.onclick=()=>{v.classList.toggle("ih-header-menu-open")});const f=document.querySelector("#MENU_NAV_JS");f&&(f.onclick=e=>{e.target.tagName==="A"&&v.classList.remove("ih-header-menu-open")});const l=document.querySelector("#MENU_NAV_JS"),M=()=>` <li class="ih-menu-item" data-section="about-me" data-lang="header.menu.about_me" type="button">
             ПРО МЕНЕ
           </li>
-          <li class="menu-item" data-section="quote" type="button">
+          <li class="ih-menu-item" data-section="quote" data-lang="header.menu.quote" type="button">
             ВАРТІСТЬ
           </li>
-          <li class="menu-item" data-section="rewiews" type="button">
+          <li class="ih-menu-item" data-section="reviews"
+          data-lang="header.menu.reviews" type="button">
             ВІДГУКИ
           </li>
-          <li class="menu-item" data-section="contacts" type="button">
+          <li class="ih-menu-item" data-section="contacts" data-lang="header.menu.contacts" type="button">
             КОНТАКТИ
-          </li>`;l&&(l.onclick=e=>{if(e.target.dataset.section){const t=e.target.dataset.section,r=document.getElementById(t);console.log(e.target.dataset.section),r&&r.scrollIntoView({behavior:"smooth",block:"end",inline:"nearest"})}});const M=()=>{l&&(l.innerHTML=y())};M();const c=document.querySelector("#CUSTOM_SELECT_JS"),i=document.querySelector("#CUSTOM_SELECT_SMALL_JS"),d=["UA","EN"],a={current:d[0]},E=(e,t)=>`
-    <div data-value="${e}" class="custom-select-option" style="
+          </li>`;l&&(l.onclick=e=>{if(e.target.dataset.section){const t=e.target.dataset.section,r=document.getElementById(t);console.log(e.target.dataset.section),r&&r.scrollIntoView({behavior:"smooth",block:"end",inline:"nearest"})}});const E=()=>{l&&(l.innerHTML=M())};E();const i=document.querySelector("#CUSTOM_SELECT_JS"),c=document.querySelector("#CUSTOM_SELECT_SMALL_JS"),m=Object.keys(S),a={current:m[0]},w=(e,t)=>`
+    <div data-value="${e}" class="ih-custom-select-option" style="
     ${a.current===e?"background:transparent; ":""}order:${a.current===e?0:t}">${e}</div>
-`,p=()=>`
-   <svg class="custom-select-icon">
-            <use href="./images/icons-sprite.svg#icon-switch-arrow"></use>
+`,g=()=>`
+   <svg class="ih-custom-select-icon">
+            <use href="/images/ih-icons-sprite.svg#icon-switch-arrow"></use>
           </svg>
-    ${d.map((e,t)=>E(e,t+1)).join("")}
+    ${m.map((e,t)=>w(e,t+1)).join("")}
 
-`,v=()=>`
-        ${d.map(e=>`<div data-value="${e}" class="select-lang-btn${a.current===e?" select-lang-btn-active":""}">${e}</div>`).join(`<svg class="vertical-line">
-           <use href="./images/icons-sprite.svg#icon-vertical-line"></use>
+`,p=()=>`
+        ${m.map(e=>`<div data-value="${e}" class="ih-select-lang-btn${a.current===e?" select-lang-btn-active":""}">${e}</div>`).join(`<svg class="ih-vertical-line">
+           <use href="/images/ih-icons-sprite.svg#icon-vertical-line"></use>
          </svg>`)}
-`,L=()=>{c&&(c.innerHTML=p(),c.onclick=e=>{c.classList.toggle("custom-select-open");const t=e.target.dataset.value;t&&t!==a.current&&(a.current=t,c.innerHTML=p())})},_=()=>{i&&(i.innerHTML=v(),i.onclick=e=>{const t=e.target.dataset.value;t&&t!==a.current&&(a.current=t,i.innerHTML=v())})};_();L();const S=document.querySelector("#MENU_SOCIAL_JS"),b=()=>`  <a
-         class="menu-social-item"
+`,L=()=>{i&&(i.innerHTML=g(),i.onclick=e=>{i.classList.toggle("ih-custom-select-open");const t=e.target.dataset.value;t&&t!==a.current&&(a.current=t,y(t),i.innerHTML=g())})},_=()=>{c&&(c.innerHTML=p(),c.onclick=e=>{const t=e.target.dataset.value;t&&t!==a.current&&(a.current=t,y(t),c.innerHTML=p())})};_();L();const h=document.querySelector("#MENU_SOCIAL_JS"),q=()=>`  <a
+         class="ih-menu-social-item"
          target="_blank"
          rel="noopener"
          href="https://t.me/@alyona_alyona"
@@ -71,7 +32,7 @@
          TELEGRAM
        </a>
        <a
-         class="menu-social-item"
+         class="ih-menu-social-item"
          target="_blank"
          rel="noopener"
          href="https://www.instagram.com/lolaserenity?igsh=MWxpdGRwM2R2M3Rxcg=="
@@ -79,11 +40,11 @@
          INSTAGRAM
        </a>
        <a
-         class="menu-social-item"
+         class="ih-menu-social-item"
          target="_blank"
          rel="noopener"
          href="https://www.facebook.com/alyona.stulina"
        >
          FACEBOOK
-       </a>`,w=()=>{S&&S.insertAdjacentHTML("beforeend",b())};w();const A={ua:{header:{},hero:{},"about-me":{},advice:{},quote:{},opportunities:{},requirements:{},workflow:{},assessment:{},reviews:{},footer:{},modal:{},"pop-up":{},formats:{title:"German title",button:{test:"Зписатися"}}},en:{header:{},hero:{},"about-me":{},advice:{},quote:{},opportunities:{},requirements:{},workflow:{},assessment:{},reviews:{},footer:{},modal:{},"pop-up":{},formats:{title:"Formats studing",button:{test:"Lessons!!!"}}}},N="en";function C(e,t){return t.split(".").reduce((o,n)=>o?o[n]:void 0,e)}function T(){document.querySelectorAll("[data-lang]").forEach(t=>{const r=t.getAttribute("data-lang"),o=C(A[N],r);o&&(t.textContent=o)})}T();
+       </a>`,A=()=>{h&&h.insertAdjacentHTML("beforeend",q())};A();
 //# sourceMappingURL=commonHelpers.js.map
