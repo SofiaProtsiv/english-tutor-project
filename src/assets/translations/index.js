9086c93868c4f8957801240c804b7d0388ed0d58
zhornyk-maria <zhornyk.maria@gmail.com>
2024-03-26 13:53:22 +0200

    feat(workflow): ET-104 add localization for ua

--- src/assets/translations/index.js
+++ src/assets/translations/index.js
@@ -7,7 +7,42 @@ const translations = {
     "quote": {},
     "opportunities": {},
     "requirements": {},
-    "workflow": {},
+    "workflow": {
+      "section": {
+        "container": {
+          "title": "Як ми будемо працювати",
+          "content": {
+            "content-list": {
+              "list": {
+                "item1": {
+                  "title1": "Оцінюємо",
+                  "list1": {
+                    "item1": "ваш поточний рівень",
+                    "item2": "ваші сильні та слабкі сторони",
+                    "item3": "ваші потреби"
+                  }
+                },
+                "item2": {
+                  "title2": "Ви отримуєте",
+                  "list2": {
+                    "item1": "індивідуальний план навчання з урахуванням вашого запиту",
+                    "item2": "необхідні онлайн-матеріали",
+                    "item3": "підтримку та розвиток ваших здібностей та скілів"
+                  }
+                },
+                "item3": {
+                  "title3": "Результат",
+                  "list3": {
+                    "item1": "задоволення від навчання",
+                    "item2": "впевненість у володінні мовою"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
     "assessment": {},
     "reviews": {},
     "footer": {},
@@ -28,7 +63,42 @@ const translations = {
     "quote": {},
     "opportunities": {},
     "requirements": {},
-    "workflow": {},
+    "workflow": {
+      "section": {
+        "container": {
+          "title": "",
+          "content": {
+            "content-list": {
+              "list": {
+                "item1": {
+                  "title1": "",
+                  "list1": {
+                    "item1": "",
+                    "item2": "",
+                    "item3": ""
+                  }
+                },
+                "item2": {
+                  "title2": "",
+                  "list2": {
+                    "item1": "",
+                    "item2": "",
+                    "item3": ""
+                  }
+                },
+                "item3": {
+                  "title3": "",
+                  "list3": {
+                    "item1": "",
+                    "item2": ""
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
     "assessment": {},
     "reviews": {},
     "footer": {},
