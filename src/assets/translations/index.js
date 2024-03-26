5ff50a8cafaac7006d787264f12ee042090b0f86
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-03-26 14:26:02 +0200

    feat(advice): ET-110 add translations advice

--- src/assets/translations/index.js
+++ src/assets/translations/index.js
@@ -3,7 +3,15 @@ const translations = {
     "header": {},
     "hero": {},
     "about-me": {},
-    "advice": {},
+    "advice": {
+      "title": "Щоб вивчення мови було цікавим та захоплюючим",
+      "list": {
+        "list_item_1": "Використовую різноманітну кількість ресурсів: книги, відео, аудіоподкасти, мобільні додатки та інтерактивні вправи, щоб уникнути монотонності",
+        "list_item_2": "Навчаю різноманітними методами і роблю це інтерактивно: на заняттях ми практикуємо рольові ігри, діалоги, віртуальні екскурсії, дебати та імпровізації, щоб зробити навчання більш захоплюючим",
+        "list_item_3": "Готую до подорожей та побутових тем: розумію наскільки важливо вміти використовувати мову у реальних ситуаціях та спілкуватися з носіями мови",
+        "list_item_4": "Спілкуюся та надаю менторську підтримку: розуміючи запит та потенціал студента, я скеровую його для отримання оптимального результату. А інколи робота перетворюється паралельно в терапевтичну сесію. Наприклад, коли день жахливий, а поспілкуєшся з викладачем англійської і стає легше :)" 
+      }
+    },
     "quote": {},
     "opportunities": {},
     "requirements": {},
@@ -24,7 +32,15 @@ const translations = {
     "header": {},
     "hero": {},
     "about-me": {},
-    "advice": {},
+    "advice": {
+      "title": "Language learning should be an interesting and exciting",
+      "list": {
+        "list_item_1": "Use a variety of resources: books, videos, audio podcasts, mobile applications and interactive exercises to avoid monotony",
+        "list_item_2": "Teach in a variety of ways and do it interactively: in classes we practice role-playing, dialogues, virtual tours, debates and improvisations to make learning more exciting",
+        "list_item_3": "Preparing for travel and everyday topics: I understand how important it is to be able to use the language in real situations and communicate with native speakers",
+        "list_item_4": "I communicate and provide mentoring support: understanding the student's request and potential, I guide him to obtain an optimal result. And sometimes the work turns into a therapy session in parallel. For example, when the day is terrible, but you talk to the English teacher and it becomes easier. This is proved by my students :)"
+      }
+    },
     "quote": {},
     "opportunities": {},
     "requirements": {},
