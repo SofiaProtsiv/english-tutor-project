4503c6eedb6135ed6d92c754afbd186975c39783
KarinaSnitsarenko <143505937+KarinaSnitsarenko@users.noreply.github.com>
2024-03-26 15:38:35 +0200

    fiat(about-me) ET-109 add translation section and delete one margin

--- src/assets/translations/index.js
+++ src/assets/translations/index.js
@@ -1,46 +1,64 @@
 const translations = {
   ua: {
-    "header": {},
-    "hero": {},
-    "about-me": {},
-    "advice": {},
-    "quote": {},
-    "opportunities": {},
-    "requirements": {},
-    "workflow": {},
-    "assessment": {},
-    "reviews": {},
-    "footer": {},
-    "modal": {},
+    header: {},
+    hero: {},
+    "about-me": {
+      title: "Я альона — твій вчитель англійської",
+      paragraph: {
+        bold: "Сама того не розуміючи, я стала поліглотом.",
+        textOne:
+          "Я сама постійно вивчаю мови і можу спілкуватися на шістьох з них. Саморозвиток – це те, що рухає мене вперед і допомагає віднаходити сенси. Я реалізовую потенціал свого розвитку, подорожуючи світом мови, і вчу робити це своїх студентів. Кожне слово, яке ви вивчаєте, відкриває двері до нових можливостей та перспектив.",
+        textTwo:
+          "Саморозвиток через вивчення мови - це подорож у світ розширення горизонтів, розуміння власних можливостей і амбіцій, збільшення спроможності взаємодіяти з різними культурами. Відкрийте для себе нові світи через вивчення мови і перетворіть свій саморозвиток на незабутню подорож.",
+      },
+    },
+    advice: {},
+    quote: {},
+    opportunities: {},
+    requirements: {},
+    workflow: {},
+    assessment: {},
+    reviews: {},
+    footer: {},
+    modal: {},
     "pop-up": {},
-    "formats": {
-      "title": "German title",
-      "button": {
-        "test": "Зписатися"
-      }
-    }
+    formats: {
+      title: "German title",
+      button: {
+        test: "Зписатися",
+      },
+    },
   },
   en: {
-    "header": {},
-    "hero": {},
-    "about-me": {},
-    "advice": {},
-    "quote": {},
-    "opportunities": {},
-    "requirements": {},
-    "workflow": {},
-    "assessment": {},
-    "reviews": {},
-    "footer": {},
-    "modal": {},
+    header: {},
+    hero: {},
+    "about-me": {
+      title: "I am Alyona - yours English teacher",
+      paragraph: {
+        bold: "Without even thinking of it, I became a polyglot",
+        textOne:
+          "I constantly study languages and can communicate in six of them. Self-development is what moves me forward and helps me find meaning. I realized the potential of my development by traveling the world of language, and I teach my students to do the same. Every word you learn opens the door to new possibilities and perspectives.",
+        textTwo:
+          "Self-development through language learning is a journey into the world of expanding horizons, understanding one's own capabilities and ambitions, and increasing the ability to interact with different cultures. Discover new worlds through language learning and turn your self-development into an unforgettable journey.",
+      },
+    },
+    advice: {},
+    quote: {},
+    opportunities: {},
+    requirements: {},
+    workflow: {},
+    assessment: {},
+    reviews: {},
+    footer: {},
+    modal: {},
     "pop-up": {},
-    "formats": {
-      "title": "Formats studing",
-      "button": {
-        "test": "Lessons!!!"
-      }
-    }
-  }
-}
+    formats: {
+      title: "Formats studing",
+      button: {
+        test: "Lessons!!!",
+      },
+    },
+  },
+};
 
-export default translations
+export default translations;
