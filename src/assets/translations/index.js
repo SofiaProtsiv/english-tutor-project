4afdf2334da787e34969cf5b6b7d6a3389465aec
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-03-26 08:56:26 +0100

    feat(translation):ET-101-Hero

--- src/assets/translations/index.js
+++ src/assets/translations/index.js
@@ -1,46 +1,56 @@
 const translations = {
   ua: {
-    "header": {},
-    "hero": {},
+    header: {},
+    hero: {
+      "main-title": "давай вивчати англійську разом",
+      "about-me":
+        "Я - вчитель, із яким ви будете активно спілкуватися, ділитися посмішками, вивчати захоплюючі теми та підвищувати рівень англійської.",
+      "btn-connect": "записатися",
+    },
     "about-me": {},
-    "advice": {},
-    "quote": {},
-    "opportunities": {},
-    "requirements": {},
-    "workflow": {},
-    "assessment": {},
-    "reviews": {},
-    "footer": {},
-    "modal": {},
+    advice: {},
+    quote: {},
+    opportunities: {},
+    requirements: {},
+    workflow: {},
+    assessment: {},
+    reviews: {},
+    footer: {},
+    modal: {},
     "pop-up": {},
-    "formats": {
-      "title": "German title",
-      "button": {
-        "test": "Зписатися"
-      }
-    }
+    formats: {
+      title: "German title",
+      button: {
+        test: "Зписатися",
+      },
+    },
   },
   en: {
-    "header": {},
-    "hero": {},
+    header: {},
+    hero: {
+      "main-title": "let's study English together",
+      "about-me":
+        "I am a teacher with whom you will actively communicate, share smiles, learn exciting topics and improve your English.",
+      "btn-connect": "sign up",
+    },
     "about-me": {},
-    "advice": {},
-    "quote": {},
-    "opportunities": {},
-    "requirements": {},
-    "workflow": {},
-    "assessment": {},
-    "reviews": {},
-    "footer": {},
-    "modal": {},
+    advice: {},
+    quote: {},
+    opportunities: {},
+    requirements: {},
+    workflow: {},
+    assessment: {},
+    reviews: {},
+    footer: {},
+    modal: {},
     "pop-up": {},
-    "formats": {
-      "title": "Formats studing",
-      "button": {
-        "test": "Lessons!!!"
-      }
-    }
-  }
-}
+    formats: {
+      title: "Formats studing",
+      button: {
+        test: "Lessons!!!",
+      },
+    },
+  },
+};
 
 export default translations
