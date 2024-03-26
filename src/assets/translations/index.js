aee9e8c25a8a015278541547d9e4254cf80a7a54
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-03-26 17:37:36 +0200

    feat(requirements):ET-103 update translations, add data-lang to requi… (#17)
    * feat(requirements):ET-103 update translations, add data-lang to requirements
    
    * fix(requirements):ET-103 delete title height and update translations

--- src/assets/translations/index.js
+++ src/assets/translations/index.js
@@ -26,7 +26,21 @@ const translations = {
     },
     quote: {},
     opportunities: {},
-    requirements: {},
+    requirements: {
+      title: "Вивчення мови неможливе без",
+      firstSubtitle: "Мотивації",
+      firstArticle:
+        "Зацікавленість і бажання вивчати нову мову є ключовими для досягнення успіху.",
+      secondSubtitle: "Дисципліни",
+      secondArticle:
+        "Вивчення мови, що перетворюється на звичку і систематичний підхід допомагають у закріпленні навичок і досягненні успіху.",
+      thirdSubtitle: "Практики",
+      thirdArticle:
+        "Регулярна практика — ключ до історії успіху. І це не лише англійської стосується.",
+      fourthSubtitle: "Терпіння",
+      fourthArticle:
+        "Розуміння того, що вивчення мови — це процес, що вимагає часу та наполегливості.",
+    },
     workflow: {},
     assessment: {},
     reviews: {},
@@ -67,7 +81,21 @@ const translations = {
     },
     quote: {},
     opportunities: {},
-    requirements: {},
+    requirements: {
+      title: "Language learning is impossible without",
+      firstSubtitle: "Motivation",
+      firstArticle:
+        "Interest and desire to learn a new language are key to success.",
+      secondSubtitle: "Discipline",
+      secondArticle:
+        "Learning a language that becomes a habit and a systematic approach helps in consolidating skills and achieving success.",
+      thirdSubtitle: "Practice",
+      thirdArticle:
+        "Regular practice is the key to a success story. And this applies not only to English.",
+      fourthSubtitle: "Patience",
+      fourthArticle:
+        "Understanding that learning a language is a process that requires time and perseverance.",
+    },
     workflow: {},
     assessment: {},
     reviews: {},
