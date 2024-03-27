bfd82e2f03dad9224b44fc424745c2faca558350
zhornyk-maria <zhornyk.maria@gmail.com>
2024-03-27 17:00:35 +0200

    fix(workflow): ET - 104 fix styles (#29)
    * feat(workflow): ET-104 add localization for ua
    
    * fix(workflow): ET-104 fix styles and add new images
    
    * fix(workflow): ET-104 bug fix semantic html

--- src/assets/translations/index.js
+++ src/assets/translations/index.js
@@ -75,7 +75,42 @@ const translations = {
       fourthArticle:
         "Розуміння того, що вивчення мови — це процес, що вимагає часу та наполегливості.",
     },
-    workflow: {},
+    "workflow": {
+      "section": {
+        "container": {
+          "title": "Як ми будемо працювати",
+          "content": {
+            "content-list": {
+              "list": {
+                "item1": {
+                  "title1": "Оцінюємо",
+                  "list1": {
+                    "item1": "ваш поточний рівень",
+                    "item2": "ваші сильні та слабкі сторони",
+                    "item3": "ваші потреби"
+                  }
+                },
+                "item2": {
+                  "title2": "Ви отримуєте",
+                  "list2": {
+                    "item1": "індивідуальний план навчання з урахуванням вашого запиту",
+                    "item2": "необхідні онлайн-матеріали",
+                    "item3": "підтримку та розвиток ваших здібностей та скілів"
+                  }
+                },
+                "item3": {
+                  "title3": "Результат",
+                  "list3": {
+                    "item1": "задоволення від навчання",
+                    "item2": "впевненість у володінні мовою"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
     assessment: {},
     reviews: {},
     footer: {},
@@ -204,7 +239,42 @@ const translations = {
       fourthArticle:
         "Understanding that learning a language is a process that requires time and perseverance.",
     },
-    workflow: {},
+    "workflow": {
+      "section": {
+        "container": {
+          "title": "",
+          "content": {
+            "content-list": {
+              "list": {
+                "item1": {
+                  "title1": "",
+                  "list1": {
+                    "item1": "",
+                    "item2": "",
+                    "item3": ""
+                  }
+                },
+                "item2": {
+                  "title2": "",
+                  "list2": {
+                    "item1": "",
+                    "item2": "",
+                    "item3": ""
+                  }
+                },
+                "item3": {
+                  "title3": "",
+                  "list3": {
+                    "item1": "",
+                    "item2": ""
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
     assessment: {},
     reviews: {},
     footer: {},
