8353877d66493bbd27ba581a20405c6f9c34cdcf
Zymbsis <143128838+Zymbsis@users.noreply.github.com>
2024-03-27 10:35:51 +0100

    feat(opportunities): ET-102 adding images and icon (#3)
    * feat(opportunities): ET-102 adding images and icon
    
    * feat(opportunities)ET-102 adding picture markup
    
    * fix(opportunities): ET-102 fix picture markup
    
    * feat(opportunities): ET-102 create markup
    
    * feat(opportunities): ET-102 added css and js
    
    * fix(opportunities): ET-102 minor fix js
    
    * fix(opportunities):ET-102 minor fix css and js
    
    * fix(opportunities):ET-102 minor fix css and js
    
    * fix(opportunities):ET-102 fixed title education opportunities
    
    * fix(opportunities): ET-102 minor fix icon
    
    * feat(opportunities): ET-102 added uk-translation and attributes data-lang
    
    * fix(opportunities):ET-102 solved problem with resize
    
    * fix(opportunities):ET-102 code refactoring
    
    * fix(opportunities):ET-102 code refactoring
    
    * fix(opportunities):ET-102 code refactoring
    
    * fix(opportunities):ET-102 swiper fix
    
    * fix(opportunities):ET-102 fixed button
    
    ---------
    
    Co-authored-by: Zymbsis-fs <Oleksandra-fsdev@gmail.com>

---
+++ src/js/opportunities-slider.js
@@ -0,0 +1,62 @@
+// import Swiper from "swiper/bundle";
+// import "swiper/css/bundle";
+
+const oppTextSwiper = new Swiper(".opp-text-swiper", {
+  allowTouchMove: false,
+  loop: true,
+  speed: 1800,
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: 20,
+
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      translate: [0, "260px", 0],
+      opacity: 0,
+    },
+    next: {
+      translate: ["384px", 0, 0],
+      opacity: 0,
+    },
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+});
+
+const oppImgSwiper = new Swiper(".opp-img-swiper", {
+  navigation: { prevEl: ".opp-button-prev", nextEl: ".opp-button-next" },
+  loop: true,
+  speed: 1800,
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+  },
+  on: {
+    slideChangeTransitionStart: function () {
+      document
+        .querySelectorAll(".opp-img-wrapper")
+        .forEach((item) => item.classList.add("img-swiper-transition"));
+    },
+    slideChange: function () {
+      oppTextSwiper.slideToLoop(this.realIndex, 1800);
+    },
+  },
+});
