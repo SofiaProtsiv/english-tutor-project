c2f0c84b02aa5398b66a35ff8dbab4563ca50b04
Sofia Protsiv <prochivsofia@gmail.com>
2024-04-01 13:21:54 +0300

    fix: update id for form

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,11 +10,10 @@ const formUser = document.querySelector(".forma-for-connect");
 let firstBut = document.querySelector(".first-item-list");
 const boxFb = document.querySelector(".box-fb");
 let textArea = document.querySelector(".user-textarea");
-const TOKEN = "6077606622:AAG6g12itzLvnsQfazmk9-oBfkHb1kflQYk";
-let IDCZAT = -1002080915692;
-const NAME_PATTERN =  /^[a-zA-Zа-яА-ЯёЁґҐіІїЇєЄ]+$/;
+const TOKEN = "6794845207:AAFGkPMlyYK2gotnRYZ4sY5PIO7-HYRPQDM";
+let IDCHAT = -1001956750616;
+const NAME_PATTERN = /^[a-zA-Zа-яА-ЯёЁґҐіІїЇєЄ]+$/;
 const PHONE_PATTERN = /^\+[0-9]{11,12}$/;
-
 
 // show modal window
 buttonClick.forEach(but => {
@@ -28,10 +27,10 @@ function clearFormFields() {
     const title = document.querySelector(".title-form");
     textArea = document.querySelector(".user-textarea");
     inputsHS.forEach(input => {
-        input.value = ""; 
+        input.value = "";
     });
     textArea.value = "";
-    title.textContent === "Contact with me"? firstButton.textContent = "Choose a format study" : firstButton.textContent = "Оберіть варіант навчання";
+    title.textContent === "Contact with me" ? firstButton.textContent = "Choose a format study" : firstButton.textContent = "Оберіть варіант навчання";
     firstButton.classList.remove("first-item-list-active");
 }
 
@@ -56,12 +55,12 @@ function closeModal(e) {
 }
 
 // settings for my select
-mdlContent.addEventListener("click", function(event) {
+mdlContent.addEventListener("click", function (event) {
     if (event.target.matches(".user-btn")) {
         const selectList = event.target.nextElementSibling;
         selectList.classList.toggle("list-item-select-visible");
     }
-    
+
     else if (event.target.matches(".list-item-select-opt")) {
         firstBut = event.target.closest(".new-select").querySelector(".user-btn");
         const selectList = mdlContent.querySelector(".list-item-select");
@@ -104,8 +103,8 @@ async function handleSubmit(e) {
     const userName = formData.get("username");
     const userPhone = formData.get("phone");
     const comment = `${formData.get("comment").trim() === "" ? "Без коментарів" : formData.get("comment")}`;
-    const education = `${(firstBut.textContent === "Оберіть варіант навчання") || (firstBut.textContent === "Choose a format study")? "Формат не вибраний" : `${firstBut.textContent}`}`;
-        
+    const education = `${(firstBut.textContent === "Оберіть варіант навчання") || (firstBut.textContent === "Choose a format study") ? "Формат не вибраний" : `${firstBut.textContent}`}`;
+
     const message = `
     Нова заявка:
     1) Ім'я: ${userName};
@@ -119,7 +118,7 @@ async function handleSubmit(e) {
 async function sendMessage(message) {
     try {
         await axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-            chat_id: IDCZAT,
+            chat_id: IDCHAT,
             text: message
         });
         feedbackMessage(true);
@@ -131,7 +130,7 @@ async function sendMessage(message) {
 }
 
 //check input
-mdlContent.addEventListener("focusout", function(event) {
+mdlContent.addEventListener("focusout", function (event) {
     if (event.target.matches("input")) {
         checkInputs(event.target);
     }
@@ -166,15 +165,15 @@ function errorParagraf(classList, addOrRemove) {
 async function feedbackMessage(success) {
     const titleFeedback = document.querySelector(".title-feedback");
     success ? titleFeedback.innerHTML = `See you soon!` : titleFeedback.innerHTML = `Error`;
-    if(success) {
+    if (success) {
         document.querySelector(".p-success").style.display = "block";
         document.querySelector(".p-error").style.display = "none";
-     }
-     else {
+    }
+    else {
         document.querySelector(".p-error").style.display = "block";
         document.querySelector(".p-success").style.display = "none";
-     }
-    
+    }
+
     contentForm.style.display = "none";
     boxFb.style.display = "flex";
     form.classList.add("form-feedback");
