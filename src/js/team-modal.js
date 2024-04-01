173131e30b191341e27c9252737e95bd83600823
Zymbsis <143128838+Zymbsis@users.noreply.github.com>
2024-04-01 20:00:41 +0200

    fix(team-modal): ET-114 added webP and fixed modal height (#64)
    Co-authored-by: Zymbsis-fs <Oleksandra-fsdev@gmail.com>

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -6,16 +6,19 @@ const teamModalButtonOpen = document.querySelector(".ih-footer-team-button");
 const teamModalButtonClose = document.querySelector(".team-modal-button");
 const teamModalWindow = document.querySelector(".team-modal-backdrop");
 const teamModalList = document.querySelector(".team-modal-list");
+const teamModalContainer = document.querySelector(".team-modal-container");
 
 teamModalButtonOpen.addEventListener("click", openTeamModalHandler);
 
 function openTeamModalHandler() {
   teamModalWindow.classList.add("is-open");
   document.body.classList.add("is-hidden");
+  onWindowResize();
   teamModalButtonOpen.removeEventListener("click", openTeamModalHandler);
   teamModalButtonClose.addEventListener("click", closeTeamModalHandler);
   teamModalWindow.addEventListener("click", closeTeamModalHandler);
   document.addEventListener("keydown", closeTeamModalHandler);
+  window.addEventListener("resize", onWindowResize);
 }
 
 function closeTeamModalHandler(e) {
@@ -31,17 +34,25 @@ function closeTeamModalHandler(e) {
     teamModalButtonClose.removeEventListener("click", closeTeamModalHandler);
     teamModalWindow.removeEventListener("click", closeTeamModalHandler);
     document.removeEventListener("keydown", closeTeamModalHandler);
+    window.removeEventListener("resize", onWindowResize);
   }
 }
 
 function teamModalItemMarkup(
-  { linkedIn, devName, position, description, key, pathToPhoto },
+  {
+    linkedIn,
+    devName,
+    position,
+    description,
+    key,
+    pathToPhotoPNG,
+    pathToPhotoWebP,
+  },
   icon
 ) {
   return `<li class="team-modal-item">
-       
           <a class="team-modal-link" target="_blank" href=${linkedIn}>
-          <img class="team-modal-img" src=${pathToPhoto} width="40" height="40" alt=${description}>
+          <picture><source srcset=${pathToPhotoWebP} type="image/webp"/><img class="team-modal-img" loading="lazy" src=${pathToPhotoPNG} width="320" height="320" alt=${description} type="image/png"/></picture>
           ${icon}</a>
     
         <p class="team-modal-name" data-lang="teamModal.${key}">
@@ -53,8 +64,17 @@ function teamModalItemMarkup(
 function teamModalListMarkup(team, icon) {
   return team
     .map(
-      (item = { linkedIn, devName, position, pathToPhoto, description, key }) =>
-        teamModalItemMarkup(item, icon)
+      (
+        item = {
+          linkedIn,
+          devName,
+          position,
+          pathToPhotoPNG,
+          pathToPhotoWebP,
+          description,
+          key,
+        }
+      ) => teamModalItemMarkup(item, icon)
     )
     .join(" ");
 }
@@ -62,3 +82,17 @@ function teamModalListMarkup(team, icon) {
 teamModalList.innerHTML = teamModalListMarkup(teamList, icon);
 const currentPageLang = localStorage.getItem("lang");
 localizeElements(currentPageLang);
+
+function onWindowResize() {
+  if (
+    (window.innerWidth < 768 && window.innerHeight < 500) ||
+    (window.innerWidth >= 768 &&
+      window.innerWidth < 1440 &&
+      window.innerHeight < 550) ||
+    (window.innerWidth >= 1440 && window.innerHeight < 600)
+  ) {
+    teamModalContainer.classList.add("team-modal-container-full-screen");
+  } else {
+    teamModalContainer.classList.remove("team-modal-container-full-screen");
+  }
+}
